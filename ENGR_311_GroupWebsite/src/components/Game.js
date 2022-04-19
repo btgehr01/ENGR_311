7b2a79335f361bcd03e99a74c82bc2d126a321d5
Steven-1199 <88493064+Steven-1199@users.noreply.github.com>
2022-04-19 19:39:50 -0400

    updated game section

--- ENGR_311_GroupWebsite/src/components/Game.js
+++ ENGR_311_GroupWebsite/src/components/Game.js
@@ -2,6 +2,10 @@ import React from "react";
 import "./Game.css";
 import sample2 from "../assets/sample2.png";
 import sample3 from "../assets/sample3.png";
+import picture1 from "../assets/picture1.png";
+import picture2 from "../assets/picture2.png";
+import picture3 from "../assets/picture3.png";
+
 //#013a34
 const Game = () => {
   return (
@@ -12,45 +16,61 @@ const Game = () => {
             <div className="game-des-introduction">
               <h2>INTRODUCTION</h2>
               <p>
-                As she sat watching the world go by, something caught her eye.
-                It wasn't so much its color or shape, but the way it was moving.
-                She squinted to see if she could better understand what it was
-                and where it was going, but it didn't help. As she continued to
-                stare into the distance, she didn't understand why
+                Test yourself! This is a simple trivia game based on the videos
+                found on the website. Aim to get every question right, but if
+                you get one wrong, you’ll lose a life. Try to get through all
+                the questions before you run out of lives!
               </p>
             </div>
-            <div className="game-des-step">
+            <div className="game-des-step" style={{ backgroundColor: "black" }}>
               <div className="game-des-step-left">
                 <h2>STEP 1</h2>
-                <p>
-                  this uneasiness was building inside her body. She felt like
-                  she should get up and run. If only she could make out what it
-                  was. At that moment, she comprehended what it was and where it
-                  was heading, and she knew her life would never be the same.
-                </p>
+                <p>Select a topic based on the videos on the website!</p>
               </div>
               <div className="game-des-step-right">
-                <img alt="demo1" src={sample2} />
+                <img alt="demo1" src={picture1} />
               </div>
             </div>
             <div className="game-des-step-second">
               <div className="game-des-step-letf-second">
-                <img alt="demo2" src={sample3} />
+                <img alt="demo2" src={picture2} />
               </div>
               <div className="game-des-step-right-second">
                 <h2>STEP 2</h2>
                 <p>
-                  this uneasiness was building inside her body. She felt like
-                  she should get up and run. If only she could make out what it
-                  was. At that moment, she comprehended what it was and where it
-                  was heading, and she knew her life would never be the same.
+                  Choose your best guess to the answer at the top. Your score
+                  will be consider as a percent of right answers out of the
+                  total amount of questions in the topic and is displayed at the
+                  top left. You have five lives – that means you can only answer
+                  wrong five times. Watch your life count with the bar on the
+                  far right.
                 </p>
               </div>
             </div>
-            <div className="game-download-container shadowInner">
-              <button className="game-download-button">Download</button>
-              <h4>Download and play here</h4>
+            <div
+              className="game-des-step"
+              style={{ backgroundColor: "#282828" }}
+            >
+              <div className="game-des-step-left">
+                <h2>STEP 3</h2>
+                <p>
+                  Once you select an answer, each answer will light up based on
+                  if they are correct or not. Red means incorrect and green
+                  means correct. Once you are ready, click Next Question at the
+                  top right to move to the next question.
+                </p>
+              </div>
+              <div className="game-des-step-right">
+                <img alt="demo1" src={picture3} />
+              </div>
             </div>
+            <a
+              className="game-download-container shadowInner"
+              href="https://bryangreenwood.github.io/PhaserTriviaGame/"
+              target="_blank"
+            >
+              <h4>Click here to play</h4>
+            </a>
           </div>
         </div>
       </div>
